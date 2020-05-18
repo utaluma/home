de9b2d237e707d64290c480249c34065696465b6
utaluma <65368146+utaluma@users.noreply.github.com>
2020-05-18 21:02:24 +0300

    Add files via upload

---
+++ 224/cdn/js/countrieslist_cy-gr1.js
@@ -0,0 +1,31 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        gr: {
+            name: 'Ελλάδα',
+            code: 'gr',
+            price: 39,
+            oldPrice: 78,
+            labelPrice: 'EUR',
+            phoneHelper: 'για παράδειγμα +30 1234567890',
+            nameHelper: 'Ioanna Benaki',
+            nameError: 'Απαιτείται όνομα',
+            phoneError: 'Εσφαλμένα καθορισμένο αριθμό',
+            countryError: 'Απαιτείται χώρα',
+            isDefault: true
+        },
+        cy: {
+            name: 'Κύπρος',
+            code: 'cy',
+            price: 39,
+            oldPrice: 78,
+            labelPrice: 'EUR',
+            phoneHelper: 'για παράδειγμα +357 12345678',
+            nameHelper: 'Pantelis Lazaridis',
+            nameError: 'Απαιτείται όνομα',
+            phoneError: 'Εσφαλμένα καθορισμένο αριθμό',
+            countryError: 'Απαιτείται χώρα',
+        }
+    };
+})();
